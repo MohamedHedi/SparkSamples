ea646cbe2f14cb588a868429ab27d0932d5caf69
Mohamed Hedi ABIDI <abidimh@gmail.com>
2014-11-27 01:23:40 +0100

    SparkKMeans.scala

--- src/main/scala/com/ebiznext/spark/examples/SparkKMeans.scala
+++ src/main/scala/com/ebiznext/spark/examples/SparkKMeans.scala
@@ -35,8 +35,31 @@ object SparkKMeans extends App {
     System.exit(1)
   }
   showWarning()
-
-  //TODO : Add your code here 
-  
+  val sparkConf = new SparkConf().setAppName("SparkKMeans")
+  val sc = new SparkContext(sparkConf)
+  val lines = sc.textFile(args(0))
+  val data = lines.map(parseVector _).cache()
+  val K = args(1).toInt
+  val convergeDist = args(2).toDouble
+  val kPoints = data.takeSample(withReplacement = false, K, 42).toArray
+  var tempDist = 1.0
+  while (tempDist > convergeDist) {
+    val closest = data.map(p => (closestPoint(p, kPoints), (p, 1)))
+    val pointStats = closest.reduceByKey { case ((x1, y1), (x2, y2)) => (x1 + x2, y1 + y2) }
+    val newPoints = pointStats.map { pair =>
+      (pair._1, pair._2._1 * (1.0 / pair._2._2))
+    }.collectAsMap()
+    tempDist = 0.0
+    for (i <- 0 until K) {
+      tempDist += squaredDistance(kPoints(i), newPoints(i))
+    }
+    for (newP <- newPoints) yield {
+      kPoints(newP._1) = newP._2
+    }
+    println("Finished iteration (delta = " + tempDist + ")")
+  }
+  println("Final centers:")
+  kPoints.foreach(println)
+  sc.stop()
 
 }
